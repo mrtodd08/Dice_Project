6b2bbb9d01df39cf96bdbbc5b28ede8910f05196
mrtodd008 <mrtodd08@gmail.com>
2024-10-02 12:59:34 -0700

    Dice changes

--- diceRoll.js
+++ diceRoll.js
@@ -1,8 +1,75 @@
-function rollDice(sides) {
-  if (sides < 1) {
-    throw new Error("The number of sides must be at least 1.");
+function createDice(number) {
+  const dotPositionMatrix = {
+    1: [[50, 50]],
+    2: [
+      [20, 20],
+      [80, 80],
+    ],
+    3: [
+      [20, 20],
+      [50, 50],
+      [80, 80],
+    ],
+    4: [
+      [20, 20],
+      [20, 80],
+      [80, 20],
+      [80, 80],
+    ],
+    5: [
+      [20, 20],
+      [20, 80],
+      [50, 50],
+      [80, 20],
+      [80, 80],
+    ],
+    6: [
+      [20, 20],
+      [20, 80],
+      [50, 20],
+      [50, 80],
+      [80, 20],
+      [80, 80],
+    ],
+  };
+
+  const dice = document.createElement("div");
+
+  dice.classList.add("dice");
+
+  for (const dotPosition of dotPositionMatrix[number]) {
+    const dot = document.createElement("div");
+
+    dot.classList.add("dice-dot");
+    dot.style.setProperty("--top", dotPosition[0] + "%");
+    dot.style.setProperty("--left", dotPosition[1] + "%");
+    dice.appendChild(dot);
   }
-  return Math.floor(Math.random() * sides) + 1;
+
+  return dice;
 }
-console.log(rollDice(6));
-console.log(rollDice(6));
+
+function randomizeDice(diceContainer, numberOfDice) {
+  diceContainer.innerHTML = "";
+
+  for (let i = 0; i < numberOfDice; i++) {
+    const random = Math.floor(Math.random() * 6 + 1);
+    const dice = createDice(random);
+
+    diceContainer.appendChild(dice);
+  }
+}
+
+const NUMBER_OF_DICE = 5;
+const diceContainer = document.querySelector(".dice-container");
+const btnRollDice = document.querySelector(".btn-roll-dice");
+
+randomizeDice(diceContainer, NUMBER_OF_DICE);
+
+btnRollDice.addEventListener("click", () => {
+  const interval = setInterval(() => {
+    randomizeDice(diceContainer, NUMBER_OF_DICE);
+  }, 50);
+
+  setTimeout(() => clearInterval(interval), 1000);
+});
